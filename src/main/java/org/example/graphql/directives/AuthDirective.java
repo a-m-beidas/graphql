6f3574b0b148d4939961c3bf10ff8f3256fe5fd6
A-beidas <a.m.beidas@gmail.com>
2021-09-01 10:03:44 +0300

    Auth Directive impl

--- src/main/java/org/example/graphql/directives/AuthDirective.java
+++ src/main/java/org/example/graphql/directives/AuthDirective.java
@@ -1,27 +1,37 @@
 package org.example.graphql.directives;
 
 import graphql.schema.DataFetcherFactories;
+import graphql.schema.GraphQLArgument;
 import graphql.schema.GraphQLFieldDefinition;
+import graphql.schema.GraphQLFieldsContainer;
 import graphql.schema.idl.SchemaDirectiveWiring;
 import graphql.schema.idl.SchemaDirectiveWiringEnvironment;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.ArrayList;
 
 public class AuthDirective implements SchemaDirectiveWiring {
     @Override
     public GraphQLFieldDefinition onField(SchemaDirectiveWiringEnvironment<GraphQLFieldDefinition> env) {
 
         GraphQLFieldDefinition field = env.getElement();
-        var parentType = env.getFieldsContainer();
-
+        GraphQLArgument arg = env.getDirective().getArgument("role");
+        GraphQLFieldsContainer parentType = env.getFieldsContainer();
         var originalDataFetcher = env.getCodeRegistry().getDataFetcher(parentType, field);
         var dataFetcher = DataFetcherFactories.wrapDataFetcher(originalDataFetcher,
                 (dataFetchingEnvironment, value) -> {
-                    if (value instanceof String s) {
-                        return s.toUpperCase();
+                    if (arg.getValue().equals("ADMIN")) {
+                        SecurityContext ctx = SecurityContextHolder.getContext();
+                        Authentication auth = new UsernamePasswordAuthenticationToken(ctx.getAuthentication().getPrincipal(), ctx.getAuthentication().getCredentials(), new ArrayList<GrantedAuthority>());
+                        ctx.setAuthentication(auth);
                     }
                     return value;
                 }
         );
-
         env.getCodeRegistry().dataFetcher(parentType, field, dataFetcher);
         return field;
     }
